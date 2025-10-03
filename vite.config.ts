6c3869bd61e86a5783ce65a3a8fe08b453270f8a
@danilko.a.g <danilko.a.g@gmail.com>
2025-10-03 19:43:17 +0300

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/game/', // Важно: именно 'game' - имя вашего репозитория
+  build: {
+    // Оптимизации для игры
+    target: 'es2015',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, // Убирает console.log в продакшене
+      }
+    }
+  },
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   }
-});
+})
